fe16adda3fdd0b85c7ca1cc78562a36902460a36
Carter <carter.egan@gmail.com>
2018-10-21 18:40:45 -0500

    hoeme update

---
+++ Assets/Scripts/UnityTelloController/AutoPilot/Orbit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+    bool orbit = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+            orbit = true;
+
+        if (orbit)
+        {
+            transform.Rotate(Vector3.up,Time.deltaTime *15);
+        }
+	}
+}
